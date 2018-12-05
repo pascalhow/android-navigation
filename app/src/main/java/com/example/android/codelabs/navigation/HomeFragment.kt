eab5a80e0f36087c8bd3ac9f8abd3af6b2dcfbd4
pascalhow <pascal.how@mydrivesolutions.com>
2018-12-05 14:14:37 +0000

    Step 5 - Add Settings fragment - Hook up the navigation button on the home fragment to the step one destination

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -17,8 +17,14 @@
 package com.example.android.codelabs.navigation
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -37,10 +43,12 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-//        button?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest)
-//        }
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            //  Simply pass in the id of the fragment you want to navigate to
+            //  You can also add bundles to the navigate method
+            findNavController().navigate(R.id.flow_step_one_dest)
+        }
         //TODO END STEP 5
 
         //TODO STEP 6 - Set NavOptions
